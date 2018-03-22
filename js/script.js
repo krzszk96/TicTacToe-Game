6903979cb7bbf926a91c25b9b95d1c1e703252c5
krzszk96 <krzszk96@gmail.com>
2018-03-22 15:26:44 +0100

    added checking who won

--- js/script.js
+++ js/script.js
@@ -20,12 +20,185 @@ function whoWon(){
     }else if(event.target.textContent=="O"){
       arrayCompO.push(compid);
     }
-    console.log("tablicaX- " + arrayCompX);
-    console.log("tablicaO- " + arrayCompO);
-
+    //check if x won
+    for (var i = 0; i < arrayCompX.length; i++) {
+      if(arrayCompX[i]==1){
+        for (var j = 0; j < arrayCompX.length; j++){
+          if(arrayCompX[j]==2){
+            for (var k = 0; k < arrayCompX.length; k++){
+              if(arrayCompX[k]==3){alert("X won")}
+            }
+          }
+        }
+      }
+    }
+    for (var i = 0; i < arrayCompX.length; i++) {
+      if(arrayCompX[i]==4){
+        for (var j = 0; j < arrayCompX.length; j++){
+          if(arrayCompX[j]==5){
+            for (var k = 0; k < arrayCompX.length; k++){
+              if(arrayCompX[k]==6){alert("X won")}
+            }
+          }
+        }
+      }
+    }
+    for (var i = 0; i < arrayCompX.length; i++) {
+      if(arrayCompX[i]==7){
+        for (var j = 0; j < arrayCompX.length; j++){
+          if(arrayCompX[j]==8){
+            for (var k = 0; k < arrayCompX.length; k++){
+              if(arrayCompX[k]==9){alert("X won")}
+            }
+          }
+        }
+      }
+    }
+    for (var i = 0; i < arrayCompX.length; i++) {
+      if(arrayCompX[i]==1){
+        for (var j = 0; j < arrayCompX.length; j++){
+          if(arrayCompX[j]==4){
+            for (var k = 0; k < arrayCompX.length; k++){
+              if(arrayCompX[k]==7){alert("X won")}
+            }
+          }
+        }
+      }
+    }
+    for (var i = 0; i < arrayCompX.length; i++) {
+      if(arrayCompX[i]==2){
+        for (var j = 0; j < arrayCompX.length; j++){
+          if(arrayCompX[j]==5){
+            for (var k = 0; k < arrayCompX.length; k++){
+              if(arrayCompX[k]==8){alert("X won")}
+            }
+          }
+        }
+      }
+    }
+    for (var i = 0; i < arrayCompX.length; i++) {
+      if(arrayCompX[i]==3){
+        for (var j = 0; j < arrayCompX.length; j++){
+          if(arrayCompX[j]==6){
+            for (var k = 0; k < arrayCompX.length; k++){
+              if(arrayCompX[k]==9){alert("X won")}
+            }
+          }
+        }
+      }
+    }
+    for (var i = 0; i < arrayCompX.length; i++) {
+      if(arrayCompX[i]==1){
+        for (var j = 0; j < arrayCompX.length; j++){
+          if(arrayCompX[j]==5){
+            for (var k = 0; k < arrayCompX.length; k++){
+              if(arrayCompX[k]==9){alert("X won")}
+            }
+          }
+        }
+      }
+    }
+    for (var i = 0; i < arrayCompX.length; i++) {
+      if(arrayCompX[i]==3){
+        for (var j = 0; j < arrayCompX.length; j++){
+          if(arrayCompX[j]==5){
+            for (var k = 0; k < arrayCompX.length; k++){
+              if(arrayCompX[k]==7){alert("X won")}
+            }
+          }
+        }
+      }
+    }
+//check if O won
+for (var i = 0; i < arrayCompO.length; i++) {
+  if(arrayCompO[i]==1){
+    for (var j = 0; j < arrayCompO.length; j++){
+      if(arrayCompO[j]==2){
+        for (var k = 0; k < arrayCompO.length; k++){
+          if(arrayCompO[k]==3){alert("O won")}
+        }
+      }
+    }
+  }
+}
+for (var i = 0; i < arrayCompO.length; i++) {
+  if(arrayCompO[i]==4){
+    for (var j = 0; j < arrayCompO.length; j++){
+      if(arrayCompO[j]==5){
+        for (var k = 0; k < arrayCompO.length; k++){
+          if(arrayCompO[k]==6){alert("O won")}
+        }
+      }
+    }
+  }
+}
+for (var i = 0; i < arrayCompO.length; i++) {
+  if(arrayCompO[i]==7){
+    for (var j = 0; j < arrayCompO.length; j++){
+      if(arrayCompO[j]==8){
+        for (var k = 0; k < arrayCompO.length; k++){
+          if(arrayCompO[k]==9){alert("O won")}
+        }
+      }
+    }
+  }
+}
+for (var i = 0; i < arrayCompO.length; i++) {
+  if(arrayCompO[i]==1){
+    for (var j = 0; j < arrayCompO.length; j++){
+      if(arrayCompO[j]==4){
+        for (var k = 0; k < arrayCompO.length; k++){
+          if(arrayCompO[k]==7){alert("O won")}
+        }
+      }
+    }
+  }
+}
+for (var i = 0; i < arrayCompO.length; i++) {
+  if(arrayCompO[i]==2){
+    for (var j = 0; j < arrayCompO.length; j++){
+      if(arrayCompO[j]==5){
+        for (var k = 0; k < arrayCompO.length; k++){
+          if(arrayCompO[k]==8){alert("O won")}
+        }
+      }
+    }
+  }
+}
+for (var i = 0; i < arrayCompO.length; i++) {
+  if(arrayCompO[i]==3){
+    for (var j = 0; j < arrayCompO.length; j++){
+      if(arrayCompO[j]==6){
+        for (var k = 0; k < arrayCompO.length; k++){
+          if(arrayCompO[k]==9){alert("O won")}
+        }
+      }
+    }
+  }
+}
+for (var i = 0; i < arrayCompO.length; i++) {
+  if(arrayCompO[i]==1){
+    for (var j = 0; j < arrayCompO.length; j++){
+      if(arrayCompO[j]==5){
+        for (var k = 0; k < arrayCompO.length; k++){
+          if(arrayCompO[k]==9){alert("O won")}
+        }
+      }
+    }
+  }
+}
+for (var i = 0; i < arrayCompO.length; i++) {
+  if(arrayCompO[i]==3){
+    for (var j = 0; j < arrayCompO.length; j++){
+      if(arrayCompO[j]==5){
+        for (var k = 0; k < arrayCompO.length; k++){
+          if(arrayCompO[k]==7){alert("O won")}
+        }
+      }
+    }
+  }
+}
   });
-
-
 }
 
 function placingXO(){
